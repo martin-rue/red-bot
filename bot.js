6bd688af4ca2daa361f15e1c6c835762cf3afdf5
Maryam Islam <maryam.islam@shell.com>
2023-08-31 15:59:15 +0100

    Strategy1

--- bot.js
+++ bot.js
@@ -22,7 +22,28 @@ export const turn = ({ minesRemaining, opponentUsedRadar }) => {
   state.minesRemaining = minesRemaining;
   state.opponentUsedRadar = opponentUsedRadar;
 
-  return move(state.position.x, state.position.y);
+  if(state.position.x == 9 && !state.position.y < 9) {
+    state.position.x-=1;
+    state.position.y+=1;
+  } else if (state.position.y == 9 && !state.position.x < 9){
+    state.position.y-=1;
+    state.position.x+=1;
+  } else {
+    state.position.x+=1;
+    state.position.y+=1;
+  };
+
+  if(opponentUsedRadar) {
+    return move(state.position.x, state.position.y);
+  } else if (turn === 1) {
+    return layMine(7,2);
+  } else if (turn % 2 === 0) {
+    return runRadar(); 
+  } else {
+    return move(state.opponent.x, state.opponent.y);
+  };
+
+  // return move(state.position.x, state.position.y);
 };
 
 export const handleRadar = ({ radar }) => {
